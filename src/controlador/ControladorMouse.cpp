f227193282ffa7dc3df71d522d49a93d9193b5d2
Pablo Gotuzzo <p.gotuzzo@gmail.com>
2015-06-17 03:07:49 -0300

    Corrige bug de que el mouse seleccione aun estando fuera de la pantalla.

--- src/controlador/ControladorMouse.cpp
+++ src/controlador/ControladorMouse.cpp
@@ -8,12 +8,13 @@ ControladorMouse::ControladorMouse() {
 
 TinputGame ControladorMouse::moverMouse(SDL_Event event) {
     if(event.type == SDL_MOUSEMOTION){
-        //TODO: PASARLO A COORDENADAS LOGICAS. ESTA EN PIXELES
         posicionMouse.x = event.button.x;
         posicionMouse.y = event.button.y;
-    }
-    if(event.type == SDL_MOUSEBUTTONUP){
+    } else if(event.type == SDL_MOUSEBUTTONUP){
         if(event.button.button == SDL_BUTTON_LEFT) return TinputGame::CLICK_IZQ_MOUSE;
+    } else {
+        posicionMouse.x = -1;
+        posicionMouse.y = -1;
     }
 }
 
